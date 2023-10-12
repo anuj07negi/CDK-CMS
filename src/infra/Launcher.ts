3fcca3f4c2cf6b0bb2e006b3799f9a721a4ef4ed
anuj07negi <anujnegi07@gmail.com>
2023-10-12 20:46:05 +0530

    added pipeline stack'

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -2,9 +2,12 @@ import { App } from "aws-cdk-lib";
 import { LambdaStack } from "./stacks/lambda.stack";
 import { ApiStack } from "./stacks/api.stack";
 import { DataStack } from "./stacks/data.stack";
+import { CmsCICDStack } from "./stacks/cicd.stack";
 
 const app = new App();
 
+new CmsCICDStack(app, 'CmsCICDStack')
+app.synth();
 const dataStack = new DataStack(app, 'DataStack')
 const lambdaStack = new LambdaStack(app, 'LambdaStack', {
   cmsTable: dataStack.cmsTable
---
+++ src/infra/stacks/cicd.stack.ts
@@ -0,0 +1,20 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+
+export class CmsCICDStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+    new CodePipeline(this, 'CMSPipeline', {
+      pipelineName: 'CMSPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('anuj07negi/CDK-CMS', 'main'),
+        commands: [
+          'npx ci',
+          'npx cdk synth'
+        ]
+      })
+    })
+  }
+}
+
